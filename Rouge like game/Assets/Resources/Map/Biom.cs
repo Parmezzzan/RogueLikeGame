59d1d471094948c43a8877f372175a84edd0c26d
Ivan <I_probel@mail.ru>
2022-06-13 18:19:29 +0300

    trying commit

--- Rouge like game/Assets/Resources/Map/Biom.cs
+++ Rouge like game/Assets/Resources/Map/Biom.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+//it's a comment for commit
 [CreateAssetMenu(fileName = "Biom", menuName = "Biom", order = 1)]
 public class Biom : ScriptableObject
 {
