1be0904e7fc75d4f6c47af30abf18f169f417abd
Ivan <I_probel@mail.ru>
2022-07-27 16:46:29 +0300

    update
    update sourse of Tile and update the map generation script

--- Rouge like game/Assets/Resources/Map/Scripts/GeneratorsV2_21072022/MapProducerV2.cs
+++ Rouge like game/Assets/Resources/Map/Scripts/GeneratorsV2_21072022/MapProducerV2.cs
@@ -2,10 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public struct MapTile
+{
+    public Sprite sprite;
+    public float rare;
+}
 public class MapProducerV2 : MonoBehaviour
 {
     [SerializeField]
-    Sprite[] sprites;
+    MapTile[] sprites;
     [SerializeField]
     Transform objectRoot;
     [SerializeField]
@@ -25,8 +31,9 @@ public class MapProducerV2 : MonoBehaviour
         {
             for (int i = 0; i < tileNumX; i++)
             {
+                Sprite sprite = sprites[Random.Range(0, sprites.Length)].sprite;
                 var obj = generateTile(new Vector2() { x = posX, y = posY },
-                            sprites[Random.Range(0, sprites.Length)],
+                            sprite,
                             new Vector2(config.TileSize, config.TileSize));
                 obj.name = i.ToString() + j.ToString();
                 posX += config.TileSize;
