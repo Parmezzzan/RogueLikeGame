00bfd2fdd29866cd0220874e54dc617c90a110f8
Ivan <I_probel@mail.ru>
2022-08-01 00:03:43 +0300

    update
    update wave manager - it works with CSV files

--- Rouge like game/Assets/Resources/Map/Scripts/GeneratorsV2_21072022/WaveConfigurator.cs
+++ Rouge like game/Assets/Resources/Map/Scripts/GeneratorsV2_21072022/WaveConfigurator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 using System.Globalization;
+using System;
 
 public class WaveConfigurator : MonoBehaviour
 {
@@ -21,6 +22,7 @@ public class WaveConfigurator : MonoBehaviour
 
     private static string csvPath = "Assets/Resources/Map/Scripts/GeneratorsV2_21072022/Monsters wave description.csv";
     private string[] csvLines;
+    private WaveData[] waveDatas;
     public class WaveData
     { 
         public int waveNum { get; set; }
@@ -29,9 +31,31 @@ public class WaveConfigurator : MonoBehaviour
         public int spawnEachSec { get; set; }
         public int monstersAmount { get; set; }
         public string[] monstersName { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("#:{0},{1},{2},{3},{4},{5}",
+                            waveNum, waveBeginSec, spawnPointAmount, spawnEachSec, monstersAmount, monstersName);
+        }
     }
     private void Start()
     {
-        //csvLines = File.ReadAllLines()
+        //csvLines = new string[5];
+        csvLines = File.ReadAllLines(csvPath);
+        waveDatas = new WaveData[csvLines.Length-1];
+
+        for (int i = 0; i < csvLines.Length - 1; i++)
+        {
+            waveDatas[i] = new WaveData();
+            string[] dataString= csvLines[i+1].Split(';');
+            waveDatas[i].waveNum = Int32.Parse(dataString[0]);
+            waveDatas[i].waveBeginSec = Int32.Parse(dataString[1]);
+            waveDatas[i].spawnPointAmount = Int32.Parse(dataString[2]);
+            waveDatas[i].spawnEachSec = Int32.Parse(dataString[3]);
+            waveDatas[i].monstersAmount = Int32.Parse(dataString[4]);
+            waveDatas[i].monstersName = dataString[5].Split(',');
+
+            print(waveDatas[i].ToString());
+        }
     }
 }
