fd79224ae436c417f0e9612ce35df25f162a0685
Ivan <I_probel@mail.ru>
2022-08-07 10:43:13 +0300

    EROR
    don't use this commit

--- Rouge like game/Assets/Scripts/Enemyes/PathFinderSimple.cs
+++ Rouge like game/Assets/Scripts/Enemyes/PathFinderSimple.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PathFinderSimple : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    public Transform target;
+    //Ne ispolzyi eto ebgavno
+    private float speed = 2.0f;
+    public void Start()
     {
-        
+        speed = gameObject.GetComponent<Enemy>().enemy.speed;
     }
-
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        transform.Translate(Vector3.Normalize(target.position - transform.position) * speed * Time.deltaTime);
+    }
+    public void SetTarget(Transform tr)
+    {
+        target = tr;
     }
 }
