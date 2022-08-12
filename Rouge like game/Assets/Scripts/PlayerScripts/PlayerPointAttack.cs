ec7726e965eb945e0c339cd5e09650f7534535ef
Ivan <I_probel@mail.ru>
2022-08-12 07:53:30 +0300

    FEATURE
    new weapon!

---
+++ Rouge like game/Assets/Scripts/PlayerScripts/PlayerPointAttack.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPointAttack : MonoBehaviour
+{
+    [SerializeField]
+    private float range = 11.0f;
+    [SerializeField]
+    private float fireRate = 4.0f;
+    [SerializeField]
+    private string enemyTag = "Enemy";
+    [SerializeField]
+    private PointBullet bulletPrefab;
+    [SerializeField]
+    private Transform firePoint;
+
+    private void Start()
+    {
+        InvokeRepeating("Fire", 0.5f, 1.0f/fireRate);
+    }
+    private void Fire()
+    {
+        var colliders = Physics2D.OverlapCircleAll(transform.position, range);
+        if (colliders.Length > 1)
+        {
+            Vector3 target = Vector3.zero;
+            float minDistanse = float.MaxValue;
+
+            foreach (var item in colliders)
+            {
+                if (item.CompareTag(enemyTag))
+                {
+                    float distanse = Vector3.Distance(transform.position, item.transform.position);
+                    if (distanse < minDistanse)
+                    {
+                        minDistanse = distanse;
+                        target = item.transform.position;
+                    }
+                }
+            }
+            if (minDistanse != float.MaxValue)
+            {
+                var narrow = Vector3.Normalize(target - transform.position);
+                var bullet = Instantiate(bulletPrefab, firePoint.position, Quaternion.identity);
+                bullet.SetTargetPoint(narrow);
+            }
+        }
+    }
+}
---
+++ Rouge like game/Assets/Scripts/PlayerScripts/PointBullet.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class PointBullet : MonoBehaviour
+{
+    [SerializeField]
+    private float lifeTime = 3.0f;
+    [SerializeField]
+    private float speed = 15.0f;
+    [SerializeField]
+    private Vector3 targetNarrow = Vector3.zero;
+    [SerializeField]
+    private string tagEnemy = "Enemy";
+    [SerializeField]
+    private int damage = 15;
+
+     private void Update()
+    {
+        transform.position = transform.position + (targetNarrow * speed * Time.deltaTime);
+        lifeTime -= Time.deltaTime;
+
+        if (lifeTime < 0)
+            Destroy(gameObject);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag(tagEnemy))
+        {
+            collision.GetComponent<Enemy_HP>().TakeDamage(damage);
+            Destroy(gameObject);
+        }
+    }
+
+    /*
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag(tagEnemy))
+        {
+            collision.gameObject.GetComponent<Enemy_HP>().TakeDamage(damage);
+            Destroy(gameObject);
+        }
+    }*/
+    public void SetTargetPoint(Vector3 newTargetPoin)
+    {
+        targetNarrow = newTargetPoin;
+    }
+}
