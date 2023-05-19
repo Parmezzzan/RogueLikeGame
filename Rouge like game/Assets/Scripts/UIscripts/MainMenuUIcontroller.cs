098f1c49abfe67f983e0dded66e5b48eaef2c735
Ivan <I_probel@mail.ru>
2023-05-19 23:28:55 +0300

    FEAT
    option menu view and hide func in main Menu

--- Rouge like game/Assets/Scripts/UIscripts/MainMenuUIcontroller.cs
+++ Rouge like game/Assets/Scripts/UIscripts/MainMenuUIcontroller.cs
@@ -5,12 +5,30 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuUIcontroller : MonoBehaviour
 {
+    [SerializeField] GameObject kittenHome;
+    [SerializeField] GameObject characters;
+    [SerializeField] GameObject options;
     public void Quit()
     {
         Application.Quit();
     }
+
     public void NewGame()
     {
         SceneManager.LoadScene("Game");
+    }
+
+    public void ShowOptions()
+    {
+        options.active = true;
+        characters.active = false;
+        kittenHome.active = false;
+    }
+
+    public void HideOptions()
+    {
+        options.active = false;
+        characters.active = true;
+        kittenHome.active = true;
     }
 }
